a5b2e8988d1ab369693f0b919079ea10c86ff486
Raghu Betina <raghu@firstdraft.com>
2017-03-15 15:49:34 +0000

    Only item user can modify item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :current_user_must_be_item_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_item_user
+    item = Item.find(params[:id])
+
+    unless current_user == item.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @items = Item.all
 
