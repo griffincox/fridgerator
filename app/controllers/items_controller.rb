b03091e11ee3f81ef18bdd914619de75a28417b9
Raghu Betina <raghu@firstdraft.com>
2017-03-15 15:49:34 +0000

    Only item user can access show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :current_user_must_be_item_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_item_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_item_user
     item = Item.find(params[:id])
